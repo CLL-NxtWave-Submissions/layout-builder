35700a7c812d383031f15c773b4246b9be4f5693
Surya T N <suryatej.nim@yahoo.com>
2022-11-04 11:56:56 +0000

    Implemented the state, state change handlers and added Provider component from ConfigurationContext object, to change context with state change and trigger component re-renders for nested consumers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import {Component} from 'react'
 
+import ConfigurationContext from './context/ConfigurationContext'
+
 import ConfigurationController from './components/ConfigurationController'
 
 import Layout from './components/Layout'
@@ -7,11 +9,45 @@ import Layout from './components/Layout'
 import './App.css'
 
 class App extends Component {
+  state = {
+    showContent: true,
+    showLeftNavbar: true,
+    showRightNavbar: true,
+  }
+
+  onToggleShowContent = () =>
+    this.setState(prevAppState => ({
+      showContent: !prevAppState.showContent,
+    }))
+
+  onToggleShowLeftNavbar = () =>
+    this.setState(prevAppState => ({
+      showLeftNavbar: !prevAppState.showLeftNavbar,
+    }))
+
+  onToggleShowRightNavbar = () =>
+    this.setState(prevAppState => ({
+      showRightNavbar: !prevAppState.showRightNavbar,
+    }))
+
   render() {
+    const {showContent, showLeftNavbar, showRightNavbar} = this.state
+
     return (
       <div>
-        <ConfigurationController />
-        <Layout />
+        <ConfigurationContext.Provider
+          value={{
+            showContent,
+            showLeftNavbar,
+            showRightNavbar,
+            onToggleShowContent: this.onToggleShowContent,
+            onToggleShowLeftNavbar: this.onToggleShowLeftNavbar,
+            onToggleShowRightNavbar: this.onToggleShowRightNavbar,
+          }}
+        >
+          <ConfigurationController />
+          <Layout />
+        </ConfigurationContext.Provider>
       </div>
     )
   }
